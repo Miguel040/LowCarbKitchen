6ba3399c858d1093021385d75bb134ce97545776
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-01-27 10:32:11 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
 /*
@@ -20,3 +21,6 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register'); 
 Route::post('/register', [RegisterController::class, 'store']); 
+
+Route::get('/login', [LoginController::class, 'index'])->name('login'); 
+Route::post('/login', [LoginController::class, 'store']); 
