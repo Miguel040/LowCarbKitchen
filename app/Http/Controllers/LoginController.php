9e6182384197c850a1d582644ce214e43f854a75
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-01-27 17:33:08 +0100

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers;
 
 use view;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index()
     {
         return view('auth.login'); 
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]); 
+
+        $needsforlogin = $request->only('email','password'); 
+
+        if(Auth::attempt($needsforlogin))
+        {
+            $request->session()->regenerate();
+            return redirect()->route('home'); 
+        }
+
+        return back()->withErrors([
+            'email' => 'Deine Email Adreesse gibt es nicht.',
+        ]); 
     }
 }
