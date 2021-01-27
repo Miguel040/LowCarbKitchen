3aba7bce132729188905b5ad7104e4307391dd4d
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-01-27 18:53:13 +0100

    Update LogoutController.php

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function index()
+  
+    public function store()
     {
-        return view('auth.logout'); 
+        auth()->logout(); 
+
+        return redirect()->route('home'); 
     }
 }
