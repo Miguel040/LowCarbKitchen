7cc9adcc4806f66430e6c0e8dfe894516e57c15f
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-01-27 17:33:16 +0100

    Create logout.blade.php

---
+++ resources/views/auth/logout.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('content')
+
+
+    <div class="flex justify-center">
+        <h2>Login</h2>
+        <div class="w-6/12 mt-4 p-6 bg-white rounded-lg">
+            <h2 class="text-xl">Bye Bye</h2>
+        </div>
+    </div>
+
+@endsection
