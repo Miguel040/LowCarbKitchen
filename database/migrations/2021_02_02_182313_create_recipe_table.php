6e6ae3026fb24689601b14399b03424d111ad4fc
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-02-05 12:42:52 +0100

    Create 2021_02_02_182313_create_recipe_table.php

---
+++ database/migrations/2021_02_02_182313_create_recipe_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
+            $table->text('recipeTitel');
+            $table->text('recipeText');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe');
+    }
+}
