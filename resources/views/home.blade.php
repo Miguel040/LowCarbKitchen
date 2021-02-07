cb58ec35476a7d52c22d928c77ced0599405d020
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-02-07 14:50:20 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,44 @@
 @extends('layout.app')
 
 @section('content')
-<h2 class="mb-4">Rezept</h2>
-<div class="flex justify-center">
-
-    <div class="w-6/12 mt-4 p-6 bg-white rounded-lg">
-        <div class="relative h-40 w-2/3 bg-red-500">
-        Bild
-        </div>
-
-        <div class="mt-4 mb-4">
-            <h2>Rezept Titel</h2>
-            <div>Rezept</div>
-        </div>
-    </div>
-    
-</div>
 
 @auth
-<a href="{{'newrecipe'}}">Neues Rezept erstellen</a>
-@endauth
+    <a href="{{'newrecipe'}}" class="text-left inline-block p-3 bg-green-200 rounded-xl shadow-xl">Neues Rezept erstellen</a>
+    @endauth
+    @if($recipes->count())
+  
+    <div class="mx-auto w-6/12">
+    <h2 class="mb-4 mx-auto  mt-4  text-center">Rezepte</h2>
+
+    </div>
+    <div class=" mx-auto w-6/12">   
+@foreach($recipes as $recipe )
+    <div class="container  mt-4 p-6 bg-white rounded-lg mx-auto ">
+   
+        <p class="text-xs"> Rezept erstellt von: {{$recipe->user->username}}</p>
+
+        <img src=" {{ asset('/storage/'. $recipe->dataUpload)}}" height="50" width="50" class="relative h-auto w-2/3 rounded-lg">
+      
+        
+        <div class="mt-4 mb-4">
+            <h2 class="text-xl mb-4">{{ $recipe -> recipeTitel }}</h2>
+            <p class="underline">Zubereitung</p>
+            <div> {!! nl2br(e($recipe -> recipeText))  !!}</div>
+        </div>
+        <form action="{{route('recipes.destroy', '$recipe')}}" method="post">
+            @csrf
+            @method('DELETE')
+            <button>Dieses Rezept löschen</button>
+        </form>
+    </div>
+    @endforeach
+    {{$recipes->links()}}
+    @else 
+    <p>hat nicht geklapp</p>
+    @endif
+
+  
+</div>
+
 
 @endsection
