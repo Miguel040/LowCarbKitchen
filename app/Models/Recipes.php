d3ef18ce435ee50e555cfe6d77199b961e6d3a80
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-02-07 14:50:14 +0100

    Update Recipes.php

--- app/Models/Recipes.php
+++ app/Models/Recipes.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Recipes extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'username',
+        'user_id', 
+        'recipeTitel',
+        'recipeText',
+        'dataUpload'
+    ]; 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class); 
+    }
     
 }
