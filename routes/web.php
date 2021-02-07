6311fe0e37a297499742ea6ced7b2ef2c004ebda
Miguel040 <77977004+Miguel040@users.noreply.github.com>
2021-02-07 14:50:27 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\recipes\RecipesController;
 use App\Http\Controllers\recipes\NewRecipeController;
 use App\Http\Controllers\recipes\RanrecipeController;
 use App\Http\Controllers\recipes\FamrecipesController;
@@ -20,9 +22,11 @@ use App\Http\Controllers\recipes\FavrecipesController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+/* Route::get('/', function () {
     return view('home');
-})->name('home');
+})->name('home'); */
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register'); 
 Route::post('/register', [RegisterController::class, 'store']); 
@@ -35,5 +39,9 @@ Route::get('/logout', [LogoutController::class, 'store'])->name('logout');
 Route::get('/famrecipes', [FamrecipesController::class, 'index'])->name('famrecipes'); 
 Route::get('/ranrecipe', [RanrecipeController::class, 'index'])->name('ranrecipe');
 Route::get('/favrecipes', [FavrecipesController::class, 'index'])->name('favrecipes');  
+
 Route::get('/newrecipe', [NewRecipeController::class, 'index'])->name('newrecipe'); 
-Route::post('/newrecipe', [NewRecipeController::class, 'store']); 
+Route::post('/newrecipe', [RecipesController::class, 'store']); 
+Route::delete('/home/{recipe}',[RecipesController::class, 'destroy'])->name('recipes.destroy');
+
+
